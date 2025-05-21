013d10c4f627458554ca12670ba538edf27d2424
ablacs <arthurblcs@yahoo.com.br>
2025-05-21 11:38:33 -0300

    Seção de projetos

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,12 @@
+import Paragrafro from '../Paragrafro'
+import Title from '../TItle'
+import { Card, LinkBotao } from './style'
+
+const Projeto = () => (
+  <Card>
+    <Title>Projeto lista de tarefas</Title>
+    <Paragrafro tipo="secundario">Lista de tarefas feita com VueJS</Paragrafro>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,36 @@
+import Projeto from '../../components/Projeto'
 import Title from '../../components/TItle'
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Title fontSize={16}>Projetos</Title>
+    <Lista>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+    </Lista>
   </section>
 )
 export default Projetos
